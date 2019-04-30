aa4394eb04c28d7f2930ebd05a67d4dfc2305228
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-30 04:39:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   # your code here
-  square = []
-  array.each do |ele|
-    square.push(ele**2)
-  end
-  square
+ # square = []
+ # array.each do |ele|
+  #  square.push(ele**2)
+ # end
+  #square
+  array.collect()
 end
